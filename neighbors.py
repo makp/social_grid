3969d57bf2fe410c20465d8d8db8f1a73448296a
mak-leibniz <makmiller@gmail.com>
2021-02-04 23:31:07 -0500

    First commit

---
+++ neighbors.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+# Lines for testing:
+# lat = np.arange(9).reshape(3,3)
+# from neighbors import Neighbors
+# teste = Neighbors(lat, 4)
+# teste.list_neighbors()
+
+class Neighbors:
+    def __init__(self, array, num_neighbors):
+        self.array = array
+        self.num_neighbors = num_neighbors
+
+    def list_neighbors(self):
+        """Returns an array in which each row lists the value of every cell followed by the value of its neighbors based on the chosen type of neighborhood (von Neumann and Moore)."""
+        nbr = dic[self.num_neighbors]  # neighborhood type
+        ca = self.array                # the given CA
+        gen = (rotateCA(ca, *t) for t in nbr)
+        result = ca.ravel()
+        for g in gen:
+            result = np.vstack((result, g.ravel()))
+        return result.T
+
+# Four neighbors
+neighNeumann = {(+1, 0),        # N (immediately above)
+                (-1, 0),        # S
+                (0, -1),        # E
+                (0, +1)}        # W
+
+# Eight neighbors
+neighMoore = neighNeumann.union({
+    (+1, -1),                   # 
+    (-1, -1),                   # 
+    (-1, +1),                   # 
+    (+1, +1)})                  #
+
+dic = {4: neighNeumann, 8: neighMoore}
+
+def rotate_right(array, steps):
+    a = array[-steps:]
+    b = array[:-steps]
+    return np.concatenate((a,b))
+
+def rotateCA(array, i, j):
+    """For a given 2D array, rotate rows i steps and their elements j steps."""
+    arr = rotate_right(array, i)                  # rotate rows
+    t = tuple(rotate_right(row,j) for row in arr)  # rotate elements
+    return np.array(t)
+
---
+++ pd_game.py
@@ -0,0 +1,70 @@
+import numpy as np
+from neighbors import Neighbors
+
+# Cmds for testing:
+# from pd_game import PD; teste = PD(10, 1.4, 0.5)
+
+
+class PD:
+    def __init__(self, l, t, p):
+        self.length = l         # CA length
+        self.t_pay = t          # temptation for defecting
+        self.init = choose_init(p, l)
+        
+    def run_once(self):
+        """Select the strategy with the highest payoff in the Moore neighborhood."""
+        ca, l, t = self.init, self.length, self.t_pay
+        nbrs, pa = payoff_array(t, ca)
+        arr = Neighbors(pa,8).list_neighbors()
+        b = np.array(tuple(pick_one(row) for row in arr))
+        return nbrs[b].reshape(l, l)
+
+#
+# Choose initial array
+#
+def choose_init(p,l):
+    if 0<p<1:
+        return init_random(p,l)
+    elif p == 0:
+        return init_mid(l)
+
+def init_random(p,l):
+    """Generate a random array with 0s (defectors) and 1s (coorperators) with probability '1-prob' and 'prob,' respectively."""
+    return np.random.choice(2, size=(l,l), p=[1-p, p])
+        
+def init_mid(l):
+    """Generate array with a single defector in the middle of the grid---or approximately the middle when length is an even number."""
+    m = (l-1)//2
+    arr = np.ones((l,l))
+    arr[m,m] = 0
+    return arr
+
+#
+# Run one time step
+#
+def payoff_array(t_pay, ca):
+    """Returns a tuple with two members. The first member is an array of each cell followed by its Moore neighbors. The second member is an array storing the payoff_total for each cell."""
+    nbrs = Neighbors(ca, 8).list_neighbors()
+    l = len(ca)
+    t = tuple(payoff_total(*row, t_pay) for row in nbrs)
+    return nbrs, np.array(t).reshape(l, l) 
+
+def payoff_total(t_pay, focal, *nbrs):
+    '''Return total payoff for a focal cell due to pairwise interactions with neighbors.'''
+    total = sum((focal, *nbrs))
+    if focal == 1:              # cooperators
+        pay = total
+    elif focal == 0:            # defectors
+        pay = t_pay*total
+    return pay
+
+def pick_one(array_1d):
+    """Returns True for only of the max element found in the 1D array. If array contains multiple entries equal to the maximum value, choose one of these entries randomly."""
+    i_all = np.array(tuple(range(array_1d.size)))  # indexes
+    m = max(array_1d)                              # max value
+    b = array_1d == m
+    i_mxs = i_all[b]            # indexes for max value entries
+    if len(i_mxs) > 1:          # more than one entry
+        c = np.random.choice(i_mxs)
+        return i_all == c
+    return b

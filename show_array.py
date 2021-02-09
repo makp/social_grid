ab574868ccfaa883e734d37bcdf5e76573575b83
mak-leibniz <makmiller@gmail.com>
2021-02-09 00:06:57 -0500

    Add (incomplete) func for RGB conver with hist

--- show_array.py
+++ show_array.py
@@ -30,7 +30,19 @@ def rgb_convert(arr, dic):
         arr_new[arr==key] = val
     return arr_new
 
-
+def rgb_covert_with_hist(arrs, dic):
+    """"""
+    # To test whether history is involved, I could check the type.
+    arrs_paired = arrs          # FIXME: create function to pair arrays an arrs
+    keys = np.array(tuple(dic.keys()))
+    vals = (colors.to_rgb(val) for val in dic.values())
+    n_rows, n_colums = arrs[0].shape
+    arr_new = np.empty((n_rows, n_colums, 3))
+    gen = np.array(tuple(lower_zip(arrs[0],arrs[1])))
+    for key,val in zip(keys,vals):
+        b = np.logical_and(arrs[0]==key[0], arrs[1]==key[1])
+        arr_new[b] = val
+    return arr_new
 
 # fig, axs = plt.subplots(nrows=1, ncols=5)
 # fig.set_title("")

f97a3c03f90d1e9b81669369884ee209957c47cd
mak-leibniz <makmiller@gmail.com>
2021-02-11 23:50:06 -0500

    Revise test function for different numbers of data points

--- pd_test.py
+++ pd_test.py
@@ -8,7 +8,16 @@ vis = Show(pd.cdic, pd.cdic_hist)  # convert dics to RGB
 
 ca0 = pd.create_init()
 
-series = pd.run(ca0,10)
+n_steps = 10
+series = pd.run(ca0,n_steps)
 
-fig,axs = plt.subplots(5,2)
-vis.show_tuple(series,axs,True)
+if n_steps < 13:
+    n_rows = n_steps//3
+    arr = series
+else:
+    arr = series[::5]       # every fifth member
+    n_rows = (n_steps//5)//3
+
+fig,axs = plt.subplots(n_rows,3,figsize=(10,10))
+vis.show_tuple(arr,axs,True)
+

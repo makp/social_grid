756ffb7d469564d9ca017f9fd9ee310c37de0fa7
mak-turing <makmiller@gmail.com>
2021-02-13 11:01:08 -0500

    Rename file; Remove axis

--- run_pd-game.py
+++ run_pd-game.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 from pd_game import PD
 from show_array import Show
 
-pd = PD(2)                         # temptation
+pd = PD(2)                      # temptation
 vis = Show(pd.cdic, pd.cdic_hist)  # convert dics to RGB
 
 ca0 = pd.create_init(101,1)          # only one cheater in the middle
 
-n_steps = 14
+n_steps = 12
 series = pd.run(ca0,n_steps, 'coop_bias')
 
 if n_steps < 15:
@@ -19,4 +19,6 @@ else:
     n_rows = (n_steps//5)//3
 
 fig,axs = plt.subplots(n_rows,3,figsize=(10,10))
+[ax.axis('off') for ax in axs.flatten()]
 vis.show_tuple(arr,axs,True)
+

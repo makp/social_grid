33053a5dc3297649901ca2e9693b53b20f4ec987
mak-turing <makmiller@gmail.com>
2021-03-21 12:09:00 -0400

    Consider case when cell is empty

--- random-walk.py
+++ random-walk.py
@@ -1,20 +1,21 @@
 import numpy as np
 from cell_automata import CA
 
-ca = CA(4)
+ca = CA(4)                      # von Neumann nbr
 arr = np.zeros((5, 9))
 arr[2, 4] = 1
 
 
-def choose_cell(*nbrs):
-    lst = list(nbrs)
-    inds = range(len(lst))
-    inds_empty = [i for i in inds if lst[i] == 0]
-    if inds_empty == []:        # no empty cells
-        out = [1] + lst         # stay put
-        return out
+def walk(x, *nbrs):
+    if x == 0:
+        return list((x, *nbrs))
     else:
-        ind_choice = np.random.choice(inds_empty)
-        lst[ind_choice] = 1
-        out = [0] + lst
-        return out
+        lst = list(nbrs)
+        inds = range(len(lst))
+        inds_empty = [i for i in inds if lst[i] == 0]
+        if inds_empty == []:        # no empty cells
+            return [1]+lst          # stay put
+        else:
+            ind_choice = np.random.choice(inds_empty)
+            lst[ind_choice] = 1
+            return [0]+lst

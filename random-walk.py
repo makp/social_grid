138302aeb9c6a4b8368d84283638db7ca98f1726
mak-leibniz <makmiller@gmail.com>
2021-03-23 00:13:46 -0400

    Write function to produce indices

--- random-walk.py
+++ random-walk.py
@@ -6,6 +6,11 @@ arr = np.zeros((5, 9))
 arr[2, 4] = 1
 
 
+def make_grid(i, j):
+    a = np.indices((i, j))
+    return np.stack(a, axis=-1)
+
+
 def walk(x, *nbrs):
     if x == 0:
         return np.array((x, *nbrs))
@@ -13,8 +18,8 @@ def walk(x, *nbrs):
         lst = list(nbrs)
         inds = range(len(lst))
         inds_empty = [i for i in inds if lst[i] == 0]
-        if inds_empty == []:        # no empty cells
-            return np.array([1]+lst)          # stay put
+        if inds_empty == []:         # no empty cells
+            return np.array([1]+lst)  # stay put
         else:
             ind_choice = np.random.choice(inds_empty)
             lst[ind_choice] = 1

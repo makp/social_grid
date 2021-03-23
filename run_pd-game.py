21d4519598c4e65ec4466c59a7c84c67654dcd0f
mak-leibniz <makmiller@gmail.com>
2021-03-22 21:12:46 -0400

    Make flake8 happy; Don't import numpy since it's not used

--- run_pd-game.py
+++ run_pd-game.py
@@ -1,17 +1,18 @@
-import numpy as np
 import matplotlib.pyplot as plt
 from pd_game import PD
 from show_array import Show
 
 pd = PD(1.85)                      # temptation payoff
-# With temptation equal to 1.85, the resulting pattern is invariant across the diferent methods listed in dic_funcs. That's probably bc ties in payoff are uncommon.
+# With temptation equal to 1.85, the resulting pattern
+# is invariant across the different methods listed in
+# dic_funcs. That's probably bc ties in payoff are uncommon.
 
 vis = Show(pd.cdic, pd.cdic_hist)  # convert dics to RGB
 
-ca0 = pd.create_init(51,1)          # only one cheater in the middle
+ca0 = pd.create_init(51, 1)          # only one cheater in the middle
 
 n_steps = 12
-series = pd.run(ca0,n_steps,'indifferent')
+series = pd.run(ca0, n_steps, 'indifferent')
 
 if n_steps < 15:
     n_rows = n_steps//3
@@ -20,8 +21,8 @@ else:
     arr = series[::5]       # every fifth member
     n_rows = (n_steps//5)//3
 
-fig,axs = plt.subplots(n_rows,3,figsize=(10,10))
+fig, axs = plt.subplots(n_rows, 3, figsize=(10, 10))
 [ax.axis('off') for ax in axs.flatten()]
-vis.show_tuple(arr,axs,True)
+vis.show_tuple(arr, axs, True)
 
 plt.show()

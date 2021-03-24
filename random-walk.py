0e9759ce76546c3261dcde46cdc8cccc2896e8d8
mak-leibniz <makmiller@gmail.com>
2021-03-24 00:03:11 -0400

    Write first version random walk on a CA

--- random-walk.py
+++ random-walk.py
@@ -6,23 +6,48 @@ arr = np.zeros((5, 9))
 arr[2, 4] = 1
 
 
-def make_grid_with_nbrs(i, j):
-    a = np.indices((i, j))
+def make_nbrs_indices(arr):
+    a = np.indices(arr.shape)
     grid = np.stack(a, axis=-1)
-    nbrs = ca.list_nbrs(grid)
+    nbrs = ca.rotate_array(grid)
     return nbrs
 
 
-def walk(x, *nbrs):
-    if x == 0:
-        return np.array((x, *nbrs))
+nbrs_indices = make_nbrs_indices(arr)
+
+
+def get_nbrs_indices(index):
+    i, j = index
+    inds = [tuple(ind) for ind in nbrs_indices[:, i, j]]
+    return inds
+
+
+def update_walk(arr):
+    arr_new = np.copy(arr)
+    for index, x in np.ndenumerate(arr_new):
+        if x == 1:
+            inds = get_nbrs_indices(index)
+            nbrs = [arr[ind] for ind in inds]
+            x_new, nbrs_new = walk(nbrs)
+            arr_new[index] = x_new
+            for i, n in zip(inds, nbrs_new):
+                arr_new[i] = n
+    return arr_new * 1/2
+
+
+def update_walk_multi(arr, n):
+    out = [arr]
+    for i in range(n):
+        out.append(update_walk(out[-1]))
+    return out
+
+
+def walk(nbrs):
+    r = range(len(nbrs))
+    inds_empty = [i for i in r if nbrs[i] == 0]
+    if inds_empty:              # empty spots available
+        c = np.random.choice(inds_empty)
+        out = (0, nbrs[:c]+[2]+nbrs[c+1:])
     else:
-        lst = list(nbrs)
-        inds = range(len(lst))
-        inds_empty = [i for i in inds if lst[i] == 0]
-        if inds_empty == []:         # no empty cells
-            return np.array([1]+lst)  # stay put
-        else:
-            ind_choice = np.random.choice(inds_empty)
-            lst[ind_choice] = 1
-            return np.array([0]+lst)
+        out = (2, nbrs)         # stay put
+    return out

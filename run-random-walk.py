c2a0ddd180b6e590049b744a2f4a64ea5bb63c04
mak-leibniz <makmiller@gmail.com>
2021-03-28 20:49:48 -0400

    Animate arrays produced by RandomWalk

--- run-random-walk.py
+++ run-random-walk.py
@@ -1,6 +1,35 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
 from random_walk import RandomWalk
 
 randw = RandomWalk(4)
 
-arr = randw.make_grid(10, 4)
-randw.walk_and_update_multi(arr, 10)
+num_agents = 10
+size = 100
+time_steps = 50
+
+# Make initial array
+arr = randw.make_grid(size, num_agents)
+
+# Run simulation
+arrays = randw.walk_and_update_multi(arr, time_steps)
+
+# Display arrays
+fig, ax = plt.subplots()
+
+
+def animate():
+    ims = []
+    for a in arrays:
+        a = a.astype('float64')
+        a[a == 0] = np.nan
+        im = ax.imshow(a, vmin=0)
+        ims.append([im])
+    return ims
+
+
+ani = animation.ArtistAnimation(fig, animate())
+# ani.save('animation_random_walk.mp4')
+
+plt.show()

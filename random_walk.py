e5499c6472a5de7229f6557982049cba6c954cd9
mak-turing <makmiller@gmail.com>
2021-04-02 14:27:12 -0400

    Add docstring for class; Choose better var names

--- random_walk.py
+++ random_walk.py
@@ -3,6 +3,11 @@ from neighbor import Nbr
 
 
 class RandWalk(Nbr):
+    """Models multiple random walkers in a cellular automata.
+
+    Assumptions:
+    - Two walkers cannot occupy the same cell.
+    - Periodic boundary conditions."""
     def __init__(self, dim, num_nbrs=4):
         self.dim = dim
         self.num_nbrs = num_nbrs
@@ -14,30 +19,30 @@ class RandWalk(Nbr):
             out.append(walk(out[-1], self.ind_arr))
         return out
 
-    def make_grid(self, num_agents):
-        agents = np.arange(1, num_agents+1)
+    def make_grid(self, num_walkers):
+        walkers = np.arange(1, num_walkers+1)
         arr = np.zeros(self.dim**2, dtype=int)
-        arr[:num_agents] = agents
+        arr[:num_walkers] = walkers
         np.random.shuffle(arr)
         return arr.reshape((self.dim, self.dim))
 
 
 def walk(a, ia):
-    ind_agents = np.nonzero(a)
+    ind_walkers = np.nonzero(a)
     a_new = np.copy(a)
-    for i, j in np.nditer(ind_agents):
-        ag = a[i, j]
+    for i, j in np.nditer(ind_walkers):
+        walker = a[i, j]
         ind_nbrs = tuple(ia[:, k, i, j] for k in range(2))
         nbrs = a_new[ind_nbrs]
-        a_new[i, j], a_new[ind_nbrs] = choose(ag, nbrs)
+        a_new[i, j], a_new[ind_nbrs] = choose(walker, nbrs)
     return a_new - a
 
 
-def choose(ag, nbrs):
+def choose(walker, nbrs):
     ind_zeros = np.where(nbrs == 0)[0]
     if ind_zeros.size > 0:
         c = np.random.choice(ind_zeros)
-        nbrs[c] = ag
+        nbrs[c] = walker
     else:
-        ag *= 2
-    return ag, nbrs
+        walker *= 2
+    return walker, nbrs

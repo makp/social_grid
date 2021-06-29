21450fa94377df62e98c2a2e49ef492fc85de91d
mak-turing <makmiller@gmail.com>
2021-06-29 10:31:13 -0400

    Write func for finding overlapping clusters

--- percolation.py
+++ percolation.py
@@ -1,3 +1,6 @@
+#  FIXME:  The function `fix_overlaps' does a partial job at
+# identifying overlapping clusters.#
+
 import numpy as np
 import matplotlib.pyplot as plt
 from neighbor import Nbr
@@ -7,6 +10,14 @@ side, p0, nbr_type = 10, 0.58, 8
 np.random.seed(999)
 
 arr0 = np.random.choice([0, 1], size=(side, side), p=[1-p0, p0])
+
+
+def create_inds(shape):
+    a = np.stack(np.indices(shape), axis=-1)
+    return Nbr(4).list_nbrs(a)
+
+
+inds_nbrs = create_inds((side, side))
 
 
 def tag_clusters(arr):
@@ -20,21 +31,26 @@ def tag_clusters(arr):
             else:
                 arr2d[i, j] = tag_new
                 tag_new += 1
-    return arr2d
+    return fix_overlaps(arr2d)
 
 
-def create_inds(shape):
-    a = np.stack(np.indices(shape), axis=-1)
-    return Nbr(4).list_nbrs(a)
+def fix_overlaps(arr):
+    for (i, j), val in np.ndenumerate(arr):
+        if val > 1:
+            nbr_tags = find_nbr_tags(arr, i, j)
+            if nbr_tags.any():
+                arr[i, j] = min(nbr_tags)
+    return arr
 
 
 def find_nbr_tags(arr, i, j):
-    inds = create_inds(arr.shape)[i, j, 1:]
+    inds = inds_nbrs[i, j, 1:]
     nbrs = np.array([arr[tuple(ind)] for ind in inds])
     b = nbrs > 1
     return nbrs[b]
 
 
 arr_clusters = tag_clusters(arr0)
+# arr_clusters
 plt.imshow(arr_clusters)
 plt.show()

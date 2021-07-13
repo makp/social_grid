a968ea274d32a005ca4772dc2256a4e865929917
mak-turing <makmiller@gmail.com>
2021-07-13 13:40:43 -0400

    Run SD and save output

--- run_sd-game.py
+++ run_sd-game.py
@@ -5,20 +5,14 @@ from show_array import Show
 sd = SD(2.6)                    # temptation payoff
 vis = Show(sd.cdic, sd.cdic_hist)  # convert dics to RGB
 
-ca0 = sd.create_init(50, .3)    # create init array
+ca0 = sd.create_init(50, .8)    # create init array
 
-n_steps = 33
+n_steps = 40
 series = sd.run(ca0, n_steps, 'indifferent')
 
-if n_steps < 15:
-    n_rows = n_steps//3
-    arr = series
-else:
-    arr = series[::5]       # every fifth member
-    n_rows = (n_steps//5)//3
-
-fig, axs = plt.subplots(n_rows, 3, figsize=(10, 10))
+fig, axs = plt.subplots(1, 2, figsize=(10, 10))
 [ax.axis('off') for ax in axs.flatten()]
-vis.show_tuple(arr, axs, False)
+vis.show_tuple(series[::n_steps], axs, False)
 
+plt.tight_layout()
 plt.show()
